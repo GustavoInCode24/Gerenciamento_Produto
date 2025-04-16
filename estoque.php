3672f8487186ce2bb309226808485b04a9505239
GustavoinCode24 <gugaprogramador24@gmail.com>
2025-04-16 00:12:54 -0300

    estoque2

--- estoque.php
+++ estoque.php
@@ -3,9 +3,37 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Gerenciameto de estoque</title>
 </head>
 <body>
-                
+
+    <header>
+        <h1>Gerenciamento de estoque</h1>
+    </header>
+
+    <main>
+
+    <form action="">
+
+    </form>
+
+
+    </main>
+
 </body>
 </html>
+
+<?php 
+
+    if($_SERVER["REQUEST_METHOD"] == "POST" ){
+
+        require("produto.php");
+
+
+        $Produto1 = new produto ($nome,$codigo,$precoUnitario,$estoque,$categoria);
+
+    }
+
+
+
+?>
